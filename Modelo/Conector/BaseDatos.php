23dd49da4bafaa6f23a8eeda360ffdc9e8764b6f
Braian Ledantes <braianledantes@gmail.com>
2024-11-08 20:34:00 -0300

    fix: correct database name in BaseDatos.php

--- Modelo/Conector/BaseDatos.php
+++ Modelo/Conector/BaseDatos.php
@@ -16,7 +16,7 @@ class BaseDatos extends PDO
     {
         $this->engine = 'mysql';
         $this->host = 'localhost';
-        $this->database = 'dbcarritocompras';
+        $this->database = 'bdcarritocompras';
         $this->user = 'root';
         $this->pass = '';
         $this->debug = true;
